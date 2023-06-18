743de3648bf5c800fce4fcae70a5e132c6d1be4e
ageh <22131774+ageh@users.noreply.github.com>
2023-06-18 10:32:47 +0200

    Update version number for release

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"sort"
 )
 
-const version = "1.2.2"
+const version = "1.2.3"
 const fullVersion = "tooli " + version
 const userAgent = "ageh/tool-installer-" + version
 const helpText = "tool-installer " + version + `
